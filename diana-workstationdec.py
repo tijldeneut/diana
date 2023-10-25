03e2bb09fe6e9c7e401d51faade3a4d0846f45ec
Tijl Deneut <deneut_tijl@hotmail.com>
2023-10-25 16:19:03 +0200

    Add files via upload
    Updated for newer versions of VMware Workstation.
    They now favor Windows Credential Manager for storing vCenter/ESXi passwords

--- diana-workstationdec.py
+++ diana-workstationdec.py
@@ -15,10 +15,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" VMware Workstation password (offline) decryptor."""
+"""VMware Workstation password (offline) decryptor"""
 
-from ntpath import join
-import optparse, os, sys, re
+import optparse, os, re
 import base64, urllib.parse, hashlib, hmac
 from Crypto.Cipher import AES
 
@@ -33,14 +32,17 @@ except ImportError:
 def check_parameters(options, args):
     """Simple checks on the parameters set by the user."""
     if not args or not len(args) == 1:
-        sys.exit('[-] You must provide the VMware folder containing at least 2 files:\npreferences-private.ini and ace.dat.')
+        exit('[-] You must provide the VMware folder containing at least 2 files:\npreferences-private.ini and ace.dat.')
     if not options.masterkeydir:
-        sys.exit('[-] You must provide the user DPAPI folder, see <usage>.')
+        exit('[-] You must provide the user DPAPI folder, see <usage>.')
     if not options.sid:
         try: options.sid = re.findall(r"S-1-[0-5]-\d{2}-\d+-\d+-\d+-\d+", options.masterkeydir)[0]
-        except: sys.exit('[-] You must provide the user SID to decrypt password.')
-    if not options.password and not options.pwdhash:
-        sys.exit('[-] You must provide the user password or password hash.')
+        except: exit('[-] You must provide the user SID to decrypt password.')
+    if options.sid and not options.password and not options.pwdhash: 
+        options.pwdhash = 'da39a3ee5e6b4b0d3255bfef95601890afd80709'
+        # On older systems: options.pwdhash = '31d6cfe0d16ae931b73c59d7e0c089c0'
+    if not os.path.isfile(os.path.join(args[0],'preferences-private.ini')):
+        exit('[-] Fatal error: file \'preferences-private.ini\' not found in folder {}'.format(args[0]))
 
 def parseHosts(bData):
     lstCreds = []
@@ -69,7 +71,7 @@ def parseAce(sFilepath):
     bData = base64.b64decode(sData)
     return bData
 
-def parsePreferences(sFilepath, mkp):
+def parsePreferences(sFilepath, mkp, boolVerbose):
     # userKey (DPAPI blob with AES key, 32 bytes), keySafe (x bytes), data (x bytes)
     sUserKey = sKeySafe = sData = ''
     try: 
@@ -94,7 +96,17 @@ def parsePreferences(sFilepath, mkp):
     bKeySafe = base64.b64decode(urllib.parse.unquote(sKeySafe.split('/')[len(sKeySafe.split('/'))-1].split(',')[2].replace(')','')))
     # data
     bData = base64.b64decode(sData)
+    if boolVerbose:
+        print('[+] Decrypted User Key:       {}'.format(bUserKey.hex()))
+        print('[+] Parsed Key Safe:          {}'.format(bKeySafe.hex()))
+        print('[+] Extracted encrypted data: {}'.format(bData.hex()))
     return (bUserKey, bKeySafe, bData)
+
+def stripLastChars(bData): ## Remove trailing characters
+    if bData[-1:] == b'\n': 
+        bData = bData.rstrip(bData[-1:])
+        return bData + b'\n'
+    else: return bData.rstrip(bData[-1:])
 
 if __name__ == '__main__':
     """Utility core."""
@@ -111,12 +123,14 @@ if __name__ == '__main__':
     parser.add_option('--masterkey', metavar='DIRECTORY', dest='masterkeydir')
     parser.add_option('--sid', metavar='SID', dest='sid')
     parser.add_option('--password', metavar='PASSWORD', dest='password')
-    parser.add_option('--pwdhash', metavar='HASH', dest='pwdhash', help='Example for empth hash: da39a3ee5e6b4b0d3255bfef95601890afd80709')
+    parser.add_option('--pwdhash', metavar='HASH', dest='pwdhash', help='Example for empty hash: da39a3ee5e6b4b0d3255bfef95601890afd80709')
     parser.add_option('--credhist', metavar='FILE', dest='credhist')
+    parser.add_option('--verbose', '-v', metavar='BOOL', action='store_true', default=False, help='Be more verbose (optional)')
 
     (options, args) = parser.parse_args()
 
     check_parameters(options, args)
+    boolVerbose = True if options.verbose else False
     
     mkp = masterkey.MasterKeyPool()
     mkp.loadDirectory(options.masterkeydir)
@@ -127,30 +141,29 @@ if __name__ == '__main__':
     if options.pwdhash:
         mkp.try_credential_hash(options.sid, bytes.fromhex(options.pwdhash))
     
-    (bUserKey, bKeySafe, bData) = parsePreferences(os.path.join(args[0],'preferences-private.ini'), mkp)
-    bData3 = parseAce(os.path.join(args[0],'ace.dat'))
+    (bUserKey, bKeySafe, bEncPrefData) = parsePreferences(os.path.join(args[0],'preferences-private.ini'), mkp, boolVerbose)
 
-    # Step 1: decrypt keySafe with userKey
-    bDataStep1 = AES.new(bUserKey, AES.MODE_CBC, bKeySafe[:16]).decrypt(bKeySafe[16:-20])
-    bDataStep1 = bDataStep1.rstrip(bDataStep1[-1:]) ## strip trailing characters
+    # Step 1: decrypt keySafe with userKey (first 16 bytes are the IV, last 20 bytes are verification)
+    bDataStep1 = stripLastChars(AES.new(bUserKey, AES.MODE_CBC, bKeySafe[:16]).decrypt(bKeySafe[16:-20]))
+    if boolVerbose: print('[+] Step 1 decrypted data:    {}'.format(bDataStep1.decode()))
     ## Optional verification: 
     bVerification1 = hmac.new(bUserKey, bDataStep1, hashlib.sha1).digest()
     if not bVerification1 == bKeySafe[-20:]: exit('[-] Error during decryption of step 1; current result:\n    ' + bDataStep1.decode(errors='ignore'))
     bKeyStep2 = base64.b64decode(urllib.parse.unquote(bDataStep1.decode().split(':key=')[1]))
     
-    # Step 2: decrypt encryption.data (bData)
-    bDataStep2 = AES.new(bKeyStep2, AES.MODE_CBC, bData[:16]).decrypt(bData[16:-20])
-    bDataStep2 = bDataStep2.rstrip(bDataStep2[-1:])
+    # Step 2: decrypt "encryption.data" (bEncPrefData) from preferences-private.ini with decrypted key from keySafe
+    bDataStep2 = stripLastChars(AES.new(bKeyStep2, AES.MODE_CBC, bEncPrefData[:16]).decrypt(bEncPrefData[16:-20]))
     bVerification2 = hmac.new(bKeyStep2, bDataStep2, hashlib.sha1).digest()
-    if not bVerification2 == bData[-20:]: exit('[-] Error during decryption of step 2; current result:\n    ' + bDataStep2.decode(errors='ignore'))
+    if not bVerification2 == bEncPrefData[-20:]: exit('[-] Error during decryption of step 2; current result:\n    ' + bDataStep2.decode(errors='ignore'))
     print('[+] Host decryption successful:')
     print(urllib.parse.unquote(bDataStep2.strip(b'\n').decode()))
     print('-'*25)
     lstCreds = parseHosts(bDataStep2)
     
     # Step 3: decrypt ace.dat with a static AES 256 key
-    bDataStep3 = AES.new(bStaticKey, AES.MODE_CBC, bData3[:16]).decrypt(bData3[16:-20])
-    bDataStep3 = bDataStep3.rstrip(bDataStep3[-1:])
+    if not os.path.isfile(os.path.join(args[0],'ace.dat')): exit('[-] Error: No credentials found or VMware is using the Credential Manager. Try \'diana-creddec.py\'')
+    bData3 = parseAce(os.path.join(args[0],'ace.dat'))
+    bDataStep3 = stripLastChars(AES.new(bStaticKey, AES.MODE_CBC, bData3[:16]).decrypt(bData3[16:-20]))
     bVerification3 = hmac.new(bStaticKey, bDataStep3, hashlib.sha1).digest()
     if not bVerification3 == bData3[-20:]: exit('[-] Error during decryption of step 3; current result:\n    ' + bDataStep3.decode(errors='ignore'))
     iRounds = int(bDataStep3.split(b':rounds=')[1].split(b':')[0])
@@ -159,8 +172,7 @@ if __name__ == '__main__':
 
     # Step 4: decrypt the decrypted data from ace.dat using a derived key
     bDerivedKey = hashlib.pbkdf2_hmac('sha1', sStaticPassword.encode(), bSalt, iRounds)[:16]
-    bDataStep4 = AES.new(bDerivedKey, AES.MODE_CBC, bData4[:16]).decrypt(bData4[16:-20])
-    bDataStep4 = bDataStep4.rstrip(bDataStep4[-1:])
+    bDataStep4 = stripLastChars(AES.new(bDerivedKey, AES.MODE_CBC, bData4[:16]).decrypt(bData4[16:-20]))
     bVerification4 = hmac.new(bDerivedKey, bDataStep4, hashlib.sha1).digest()
     if not bVerification4 == bData4[-20:]: exit('[-] Error during decryption of step 4; current result:\n    ' + bDataStep4.decode(errors='ignore'))
     bPassKey = base64.b64decode(urllib.parse.unquote(bDataStep4.split(b':key=')[1].decode()))
@@ -168,8 +180,7 @@ if __name__ == '__main__':
     # Step 5: final decryption of the credential(s)
     for lstCred in lstCreds:
         bPassData = base64.b64decode(lstCred[2])
-        bPassword = AES.new(bPassKey, AES.MODE_CBC, bPassData[:16]).decrypt(bPassData[16:-20])
-        bPassword = bPassword.rstrip(bPassword[-1:])
+        bPassword = stripLastChars(AES.new(bPassKey, AES.MODE_CBC, bPassData[:16]).decrypt(bPassData[16:-20]))
         bVerification5 = hmac.new(bPassKey, bPassword, hashlib.sha1).digest()
         if not bVerification5 == bPassData[-20:]: exit('[-] Error during decryption of step 5; current result:\n    ' + bPassword.decode(errors='ignore'))
         iPasslength = int(bPassword[16:][:4].hex(),16)
